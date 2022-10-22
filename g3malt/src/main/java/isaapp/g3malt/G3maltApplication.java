4f251e2c9a71af783816cde6c02e8784819e8eb0
Aleksa Colovic <93273336+aieksa00@users.noreply.github.com>
2022-10-22 11:15:36 +0200

    Add files via upload

---
+++ g3malt/src/main/java/isaapp/g3malt/G3maltApplication.java
@@ -0,0 +1,13 @@
+package isaapp.g3malt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G3maltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G3maltApplication.class, args);
+	}
+
+}
---
+++ g3malt/src/test/java/isaapp/g3malt/G3maltApplicationTests.java
@@ -0,0 +1,13 @@
+package isaapp.g3malt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G3maltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
