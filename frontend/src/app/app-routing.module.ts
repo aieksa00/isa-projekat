44abca7c6a378262a91d80ff9851d5c7d0c3f581
dokic21 <milana.dokic.md@gmail.com>
2022-11-04 23:31:52 +0100

    Log In and registration components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from "./pages/registration-page/registration-page.component";
+import { LogInPageComponent } from "./pages/log-in-page/log-in-page.component";
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 const routes: Routes = [
-  { path: 'registrationPage', component: RegistrationPageComponent}
+  { path: 'registrationPage', component: RegistrationPageComponent},
+  { path: 'logInPage', component: LogInPageComponent},
+  { path: '', component: StartPageComponent}
 ];
 
 @NgModule({
