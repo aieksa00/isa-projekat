eeb2700af908809ec85b216cf66ae81d7d2b1f6b
dokic21 <milana.dokic.md@gmail.com>
2022-11-06 22:49:36 +0100

    User profile and linked components

---
+++ frontend/src/app/pages/blood-banks-list/blood-banks-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blood-banks-list',
+  templateUrl: './blood-banks-list.component.html',
+  styleUrls: ['./blood-banks-list.component.css']
+})
+export class BloodBanksListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/pages/user-qrcodes/user-qrcodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-qrcodes',
+  templateUrl: './user-qrcodes.component.html',
+  styleUrls: ['./user-qrcodes.component.css']
+})
+export class UserQRCodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
