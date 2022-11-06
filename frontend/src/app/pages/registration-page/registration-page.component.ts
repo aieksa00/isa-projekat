a798b7cdc4b888d7ea6f87b96a6be5cd0b6026f9
dokic21 <milana.dokic.md@gmail.com>
2022-11-06 12:30:26 +0100

    DTO-registration and log in

--- frontend/src/app/pages/registration-page/registration-page.component.ts
+++ frontend/src/app/pages/registration-page/registration-page.component.ts
@@ -1,18 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
-interface User {
-  name: String;
-  /*surname: String;
-  address: String;
-  city: String;
-  country: String;
-  phoneNumber: String;
-  jmbg: String;
-  gender: String;
-  profession: String;
-  workplace: String;
-  userType: String;*/
-}
+import { UserDTO } from '../../DTO/user-dto';
+import { UserCredentialsDTO } from '../../DTO/user-credentials-dto';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-registration-page',
@@ -23,17 +12,69 @@ interface User {
 export class RegistrationPageComponent implements OnInit {
 
   public name: String = "";
+  public surname: String = "";
+  public address: String = "";
+  public city: String = "";
+  public country: String = "";
+  public phoneNumber: String = "";
+  public jmbg: String = "";
+  public gender: String = "";
+  public profession: String = "";
+  public workplace: String = "";
+  public userType: String = "";
+
+  public email: String = "";
+  public password: String = "";
+  public confirmedPassword: String = "";
 
   constructor() { }
 
   public onSubmit() {
-    const user: User = {
-      name : this.name
+    const user: UserDTO = {
+      name : this.name,
+      surname : this.surname,
+      address : this.address,
+      city : this.city,
+      country : this.country,
+      phoneNumber : this.phoneNumber,
+      jmbg : this.jmbg,
+      gender : this.gender,
+      profession : this.profession,
+      workplace : this.workplace,
+      userType : this.userType,
+            
     }
+    if(this.comparePasswords()) {
+      const userCredentials: UserCredentialsDTO = {
+        email : this.email,
+        password : this.password,
+      }
+    console.log(userCredentials);
+    } else {
+      this.showWarningMessage();
+      this.password = "";
+      this.confirmedPassword = "";
+    }
+
     console.log(user);
   }
 
   ngOnInit(): void {
+  }
+
+  comparePasswords() : boolean {
+    if(this.password === this.confirmedPassword) {
+      return true;
+    }
+    return false;
+  }
+
+  showWarningMessage(){
+    return Swal.fire({
+      title: 'Warning',
+      text: 'Passwords must be the same',
+      icon: 'warning'
+    })
   }
 
 }
