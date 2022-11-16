5882e0aed3a28b633e70eb4edb501e247e406a3c
TatjanaGemovic <gemovic.ra105.2019@uns.ac.rs>
2022-11-16 17:42:01 +0100

    Do search and filter func

--- frontend/src/app/pages/blood-banks-list/blood-banks-list.component.ts
+++ frontend/src/app/pages/blood-banks-list/blood-banks-list.component.ts
@@ -10,13 +10,41 @@ import { BloodBankService } from 'src/app/services/blood-bank.service';
 export class BloodBanksListComponent implements OnInit {
 
   public bloodbanks: BloodBanksDTO[] = [];
+  public bloodbanksFiltered: BloodBanksDTO[] = [];
+  public search: string = '';
+  public SearchByName: string = '';
+  public filterValue: string = '';
 
   constructor(private bloodBankService: BloodBankService) { }
 
   ngOnInit(): void {
     this.bloodBankService.getBloodBanks().subscribe(res => {
       this.bloodbanks = res;
+      this.bloodbanksFiltered = this.bloodbanks;
     })
+  }
+
+  public Reset() :void{
+    this.bloodbanksFiltered = this.bloodbanks;
+  }
+
+  public SearchBloodBank(): void{
+    if(this.SearchByName == 'name'){
+        this.bloodbanksFiltered = this.bloodbanks.filter(element => {
+          return element.name.includes(this.search) || element.name.includes(this.search.toLowerCase())
+        });
+    }else{
+      this.bloodbanksFiltered = this.bloodbanks.filter(element => {
+        return element.city.includes(this.search) || element.city.includes(this.search.toLowerCase())
+      });
+    }
+  }
+
+  public Filter() :void{
+    this.bloodbanksFiltered = this.bloodbanksFiltered.filter(element => {
+      let value = element.rating == this.filterValue
+      return value;
+    });
   }
 
 }
