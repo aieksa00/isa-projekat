8bceee322b686a208948601ed9b5fc0f4b0d682c
Luka Mandic <mandic.ra101.2019@uns.ac.rs>
2022-11-17 17:57:04 +0100

    Blood bank registration bug fixed

--- g3malt/src/main/java/isaapp/g3malt/model/UserType.java
+++ g3malt/src/main/java/isaapp/g3malt/model/UserType.java
@@ -1,11 +1,14 @@
 package isaapp.g3malt.model;
 
 public enum UserType {
-   administrator,
-   staff,
-   customer;
+   administrator(0),
+   staff(1),
+   customer(2);
 	
 	private int value;
+	private UserType(int value){
+        this.value = value;
+    }
 	public int getValue() {
 	        return value;
 	    }
