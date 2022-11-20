4d873e3143662414e51bd4ea80335794847d8933
dokic21 <milana.dokic.md@gmail.com>
2022-11-20 15:27:03 +0100

    sort, search and filter blood banks

--- frontend/src/app/pages/blood-banks-list/blood-banks-list.component.ts
+++ frontend/src/app/pages/blood-banks-list/blood-banks-list.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BloodBanksDTO } from 'src/app/DTO/blood-banks-list-dto';
+import { SearchDTO } from 'src/app/DTO/search-dto';
 import { BloodBankService } from 'src/app/services/blood-bank.service';
 
 @Component({
@@ -9,89 +12,47 @@ import { BloodBankService } from 'src/app/services/blood-bank.service';
 })
 export class BloodBanksListComponent implements OnInit {
 
-  public bloodbanks: BloodBanksDTO[] = [];
-  public bloodbanksFiltered: BloodBanksDTO[] = [];
   public search: string = '';
-  public SearchByName: string = '';
+  public searchByName: string = '';
   public filterValue: string = '';
   public sortValue : String = "";
+  public searchDTO : SearchDTO = new SearchDTO();
+  public bloodBanksFiltered :  BloodBanksDTO[] = [];
 
-  constructor(private bloodBankService: BloodBankService) { }
+  constructor(private bloodBankService: BloodBankService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.bloodBankService.getBloodBanks().subscribe(res => {
-      this.bloodbanks = res;
-      this.bloodbanksFiltered = this.bloodbanks;
+      this.bloodBanksFiltered = res
     })
   }
 
+  onSubmit() {
+    this.searchDTO = {
+      search : this.search,
+      searchByName : this.searchByName,
+      filterValue : this.filterValue,
+      sortValue : this.sortValue
+    }
+
+    this.getFiltered().subscribe(res => {
+      this.bloodBanksFiltered = res;
+    })
+  }
+
+  getFiltered() : Observable<any> {
+    return this.http.post<any>("http://localhost:9090/BloodBankController/getFilteredBloodBanks", this.searchDTO);
+  }
+
   public Reset() :void{
-    this.bloodbanksFiltered = this.bloodbanks;
+    this.bloodBankService.getBloodBanks().subscribe(res => {
+      this.bloodBanksFiltered = res
+    })
     this.search = "";
-    this.SearchByName = "";
+    this.searchByName = "";
     this.filterValue = "";
     this.sortValue = "";
   }
 
-  public SearchBloodBank(): void{
-    if(this.SearchByName == 'name'){
-        this.bloodbanksFiltered = this.bloodbanks.filter(element => {
-          return element.name.includes(this.search) || element.name.includes(this.search.toLowerCase())
-        });
-    }else{
-      this.bloodbanksFiltered = this.bloodbanks.filter(element => {
-        return element.city.includes(this.search) || element.city.includes(this.search.toLowerCase())
-      });
-    }
-  }
-
-  public Filter() :void{
-    this.bloodbanksFiltered = this.bloodbanksFiltered.filter(element => {
-      let value = element.rating >= this.filterValue
-      return value;
-    });
-  }
-
-  public Sort() : void {
-    if(this.sortValue === "name") {
-      this.bloodbanksFiltered = this.bloodbanksFiltered.sort((a,b) => {
-        if (a.name > b.name) {
-            return 1;
-        }
-    
-        if (a.name < b.name) {
-            return -1;
-        }
-    
-        return 0;
-      } )
-    } else if(this.sortValue === "city") {
-      this.bloodbanksFiltered = this.bloodbanksFiltered.sort((a,b) => {
-        if (a.city > b.city) {
-            return 1;
-        }
-    
-        if (a.city < b.city) {
-            return -1;
-        }
-    
-        return 0;
-      } )
-    } else if(this.sortValue === "rating") {
-      this.bloodbanksFiltered = this.bloodbanksFiltered.sort((a,b) => {
-        if (a.rating > b.rating) {
-            return 1;
-        }
-    
-        if (a.rating < b.rating) {
-            return -1;
-        }
-    
-        return 0;
-      } )
-    }
-
-
-  }
 
 }
--- g3malt/src/main/java/isaapp/g3malt/repository/BloodBankRepository.java
+++ g3malt/src/main/java/isaapp/g3malt/repository/BloodBankRepository.java
@@ -2,8 +2,14 @@ package isaapp.g3malt.repository;
 
 import isaapp.g3malt.model.BloodBank;
 
+import org.hibernate.boot.model.source.spi.Sortable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BloodBankRepository extends JpaRepository<BloodBank, Integer>{
-    
+    @Query("from BloodBank b where lower(b.name) like CONCAT('%', lower(:name), '%') and lower(b.city) like CONCAT('%', lower(:city), '%') and b.rating >= :rating")
+    List<BloodBank> searchFilterSort(@Param("name") String searchName, @Param("city") String searchCity, @Param("rating") Double filterValue);
 }
