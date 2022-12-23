292c696920b33176f11cd14c262fc404862cd644
dokic21 <milana.dokic.md@gmail.com>
2022-12-23 04:12:42 +0100

    unschedule appointment

--- frontend/src/app/pages/user-appointments/user-appointments.component.ts
+++ frontend/src/app/pages/user-appointments/user-appointments.component.ts
@@ -1,4 +1,8 @@
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-appointments',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAppointmentsComponent implements OnInit {
 
-  constructor() { }
+  public appointments : any = []
+
+  constructor(private datePipe: DatePipe, public router: Router, private http: HttpClient) { 
+    this.getNotFree().subscribe(res => {
+      this.appointments = res;
+    })
+  }
+
+  public getNotFree() : Observable<any> {
+    return this.http.get<any>("http://localhost:9090/AppointmentController/getScheduledAppointments");
+  }
+
+  public CancelAppointment(appointment : any) : Observable<any> {
+    return this.http.post<any>("http://localhost:9090/AppointmentController/unscheduleAppointment", appointment.id);
+  }
 
   ngOnInit(): void {
+  }
+
+  public Cancel(appointment : any) {
+    this.CancelAppointment(appointment).subscribe(res => {
+      this.appointments = res;
+    });
+  }
+
+  public getWorkingHours(date : any): any {
+    return this.datePipe.transform(date,'MM-dd-yyyy hh-mm-ss' );
   }
 
 }
